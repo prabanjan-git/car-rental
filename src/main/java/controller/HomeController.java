8753edf272a0e8cd5d72bad3c777593fac91d539
root <root@ip-172-31-31-68.us-east-2.compute.internal>
2025-08-30 18:26:39 +0000

    all fixed

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.example.carrental.controller;
 
+import com.example.carrental.model.Car;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index"; // points to index.html / index.jsp / index template
+    public String home(Model model) {
+        // Sample car data
+        List<Car> cars = Arrays.asList(
+            new Car("Toyota Camry", "Sedan", 50),
+            new Car("Ford Mustang", "Coupe", 120),
+            new Car("Honda CR-V", "SUV", 80)
+        );
+
+        model.addAttribute("cars", cars);  // pass cars to template
+        return "index"; // points to index.html in templates
     }
 }
 
--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -2,27 +2,25 @@ package com.example.carrental.model;
 
 public class Car {
     private String name;
-    private String model;
+    private String type;
+    private double pricePerDay;
 
     // Constructors
     public Car() {}
-    public Car(String name, String model) {
+    public Car(String name, String type, double pricePerDay) {
         this.name = name;
-        this.model = model;
+        this.type = type;
+        this.pricePerDay = pricePerDay;
     }
 
     // Getters and Setters
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getModel() {
-        return model;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public double getPricePerDay() { return pricePerDay; }
+    public void setPricePerDay(double pricePerDay) { this.pricePerDay = pricePerDay; }
 }
 
